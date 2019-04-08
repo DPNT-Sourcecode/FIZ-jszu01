9c302e42bc44d3325f7750329590fec2c97c30c0
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 10:35:16 +0000

    Mon Apr 08 10:35:16 UTC 2019

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -3,7 +3,9 @@ require './lib/solutions/HLO/hello.rb'
 describe 'Hello' do
 
   it 'says hello to the world' do
-    
+    hello = Hello.new
+    expect(hello('World')).to eq('World')
   end
 
 end
+
