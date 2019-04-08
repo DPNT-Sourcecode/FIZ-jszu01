69a4f9092f4da3da71070eb3db7e5b0cece7786e
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 13:27:57 +0000

    Mon Apr 08 13:27:57 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -1,26 +1,28 @@
 # noinspection RubyUnusedLocalVariable
 class FizzBuzz
-@return = []
+
 
   def fizz_buzz(number)
     num_array = number.to_s.chars.map(&:to_i)
-
+    @return += ""
     if (number % 5 == 0 || num_array.include?(5)) && (number % 3 == 0 || num_array.include?(3))
-      @return.push('fizz buzz')
+      @return.'fizz buzz')
      elsif number % 5 == 0 || num_array.include?(5)
        @return.push('buzz')
      elsif number % 3 == 0 || num_array.include?(3)
       @return.push('fizz')
      else
-       @return = number
+       @return.push(number)
   end
 
-  if num_num_array.uniq.length == 1
-    @return += ' deluxe'
+  if num_array.uniq.length == 1
+    @return.push('deluxe')
   else
     @return
   end
-
+  return @return
   end
+
 end
+
 
