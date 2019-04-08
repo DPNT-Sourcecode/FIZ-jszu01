7138a94a6cbe2d444407b0aedfd372c62cd81b41
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 10:26:08 +0000

    Mon Apr 08 10:26:08 UTC 2019

--- lib/solutions/SUM/sum.rb
+++ lib/solutions/SUM/sum.rb
@@ -2,7 +2,13 @@
 class Sum
 
   def sum(x, y)
+    int  = x + y
+    if int.integer? == true
+      int
+    else
     raise 'Not implemented'
+  end 
   end
 
 end
+
--- spec/sum_spec.rb
+++ spec/sum_spec.rb
@@ -3,7 +3,9 @@ require './lib/solutions/SUM/sum.rb'
 describe 'Sum' do
 
   it 'adds the sum of two integers' do
-    
+    sum = Sum.new
+    expect(sum.sum(3,5)).to eq(8)
   end
 
 end
+
