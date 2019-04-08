3de1de3aebff2c17e1ba0f8a611cd03685f30411
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 13:42:57 +0000

    Mon Apr 08 13:42:57 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -6,11 +6,13 @@ class FizzBuzz
     num_array = number.to_s.chars.map(&:to_i)
     p num_array
     p num_array.uniq.length
-    @return = ""
-    if (number % 5 == 0 || num_array.include?(5)) && (number % 3 == 0 || num_array.include?(3)) && num_array.uniq.length != 1
+    @result = []
+
+    if num_array.uniq.length == 1
+    if (number % 5 == 0 || num_array.include?(5)) && (number % 3 == 0 || num_array.include?(3))
       @return = 'fizz buzz'
-    elsif (number % 5 == 0 || num_array.include?(5)) && (number % 3 == 0 || num_array.include?(3)) && num_array.uniq.length == 1
-      @return = 'fizz buzz deluxe'
+    elsif (number % 5 == 0 || num_array.include?(5)) && (number % 3 == 0 || num_array.include?(3)) && num_array.uniq.length != 1
+        @return = 'fizz buzz'
      elsif number % 5 == 0 || num_array.include?(5) && num_array.uniq.length != 1
        @return = 'buzz'
     elsif number % 5 == 0 || num_array.include?(5) && num_array.uniq.length == 1
@@ -31,6 +33,7 @@ class FizzBuzz
 
 
 end
+
 
 
 
