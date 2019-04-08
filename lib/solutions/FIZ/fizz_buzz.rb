d4b141de4ade88e7ca21239fb2b1d82a4843aa68
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 13:30:57 +0000

    Mon Apr 08 13:30:57 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -4,25 +4,24 @@ class FizzBuzz
 
   def fizz_buzz(number)
     num_array = number.to_s.chars.map(&:to_i)
-    @return += ""
+    @return = ""
     if (number % 5 == 0 || num_array.include?(5)) && (number % 3 == 0 || num_array.include?(3))
-      @return.'fizz buzz')
+      @return = 'fizz buzz'
      elsif number % 5 == 0 || num_array.include?(5)
-       @return.push('buzz')
+       @return = 'buzz'
      elsif number % 3 == 0 || num_array.include?(3)
-      @return.push('fizz')
+      @return = 'fizz'
      else
-       @return.push(number)
+       @return = number
   end
 
+
+  end
+  
   if num_array.uniq.length == 1
-    @return.push('deluxe')
-  else
-    @return
+    @return += 'deluxe'
   end
-  return @return
-  end
-
 end
+
 
 
