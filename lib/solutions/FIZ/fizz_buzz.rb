c1ba1e49bdded8329db406073e8e3eeb2115ee40
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 12:32:08 +0000

    Mon Apr 08 12:32:08 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -4,7 +4,7 @@ class FizzBuzz
   def fizz_buzz(number)
     num_array = number.to_s.chars.map(&:to_i)
 
-    if number % 15 == 0
+    if (number % 5 == 0 || num_array.include?(5)) && (number % 3 == 0 || num_array.include?(3))
        return 'fizz buzz'
      elsif number % 5 == 0 || num_array.include?(5)
        return 'buzz'
@@ -17,3 +17,4 @@ class FizzBuzz
   end
 
 end
+
