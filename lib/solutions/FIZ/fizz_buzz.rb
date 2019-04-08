c6a65e06cfc7701ee9bb0315ff69148adac81808
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 14:45:50 +0000

    Mon Apr 08 14:45:50 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -7,16 +7,21 @@ class FizzBuzz
     @result = ""
 
     if (number % 5 == 0 || num_array.include?(5)) && (number % 3 == 0 || num_array.include?(3))
-      @result = 'fizz buzz deluxe'
+      @result = 'fizz buzz'
      elsif number % 5 == 0 || num_array.include?(5) && num_array.uniq.length != 1
-       @result = 'buzz deluxe'
+       @result = 'buzz'
      elsif number % 3 == 0 || num_array.include?(3) && num_array.uniq.length != 1
-      @result =  'fizz deluxe'
+      @result =  'fizz'
     else
       @result = number
     end
 
-
+    if (number % 3 == 0) && num_array.include?(3) || (number % 5 == 0) && num_array.include?(5) &&  @result != 'deluxe' && number % 2 == 0
+      @result = @result + " deluxe"
+    elsif (num_array.length > 1 && num_array.uniq.length == 1) &&  @result != 'deluxe' && number % 2 != 0
+      @result = @result + " fake deluxe"
+    else
+    end
 
     return @result
     end
@@ -24,4 +29,5 @@ class FizzBuzz
 
 
   end
+
 
