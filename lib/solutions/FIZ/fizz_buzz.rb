ba6df0968dc7d5025ad3d88548f14c0cd55c35ce
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 14:42:50 +0000

    Mon Apr 08 14:42:50 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -7,25 +7,16 @@ class FizzBuzz
     @result = ""
 
     if (number % 5 == 0 || num_array.include?(5)) && (number % 3 == 0 || num_array.include?(3))
-      @result = 'fizz buzz'
+      @result = 'fizz buzz deluxe'
      elsif number % 5 == 0 || num_array.include?(5) && num_array.uniq.length != 1
-       @result = 'buzz'
+       @result = 'buzz deluxe'
      elsif number % 3 == 0 || num_array.include?(3) && num_array.uniq.length != 1
-      @result =  'fizz'
-    elsif num_array.length > 1 && num_array.uniq.length == 1 && number % 2 != 0
-      @result = 'fake deluxe'
-    elsif num_array.length > 1 && num_array.uniq.length == 1 && number % 2 == 0
-      @result = 'deluxe'
+      @result =  'fizz deluxe'
     else
       @result = number
     end
 
-    if (num_array.length > 1 && num_array.uniq.length == 1) &&  @result != 'deluxe' && number % 2 == 0
-      @result = @result + " deluxe"
-    elsif (num_array.length > 1 && num_array.uniq.length == 1) &&  @result != 'fake deluxe' && number % 2 != 0
-      @result = @result + " fake deluxe"
-    else
-    end
+
 
     return @result
     end
@@ -33,3 +24,4 @@ class FizzBuzz
 
 
   end
+
