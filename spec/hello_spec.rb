cb7e0a72d0bc2d51a2995ea31a5358749c155d4c
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 11:38:53 +0000

    Mon Apr 08 11:38:53 UTC 2019

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -2,17 +2,18 @@ require './lib/solutions/HLO/hello.rb'
 
 describe 'Hello' do
 
-  it 'says hello to the world' do
-    hello = Hello.new
-    expect(hello.hello('World')).to eq('Hello, World!')
-  end
+  # it 'says hello to the world' do
+  #   hello = Hello.new
+  #   expect(hello.hello('World')).to eq('Hello, World!')
+  # end
 
   it 'says hello to the world' do
     hello = Hello.new
-    expect(hello.hello('Friend')).to eq('Hello, Friend!')
+    expect(hello.hello('John')).to eq('Hello, John!')
   end
 
 end
+
 
 
 
