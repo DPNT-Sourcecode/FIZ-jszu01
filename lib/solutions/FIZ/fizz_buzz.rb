9809478eafe88b4851d99e3b4d599f0f5fbd7ff6
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 13:33:57 +0000

    Mon Apr 08 13:33:57 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -5,23 +5,29 @@ class FizzBuzz
   def fizz_buzz(number)
     num_array = number.to_s.chars.map(&:to_i)
     @return = ""
-    if (number % 5 == 0 || num_array.include?(5)) && (number % 3 == 0 || num_array.include?(3))
+    if (number % 5 == 0 || num_array.include?(5)) && (number % 3 == 0 || num_array.include?(3)) && num_array.uniq.length != 1
       @return = 'fizz buzz'
-     elsif number % 5 == 0 || num_array.include?(5)
+    elsif (number % 5 == 0 || num_array.include?(5)) && (number % 3 == 0 || num_array.include?(3)) && num_array.uniq.length == 1
+     elsif number % 5 == 0 || num_array.include?(5) && num_array.uniq.length != 1
+       @return = 'fizz buzz'
        @return = 'buzz'
-     elsif number % 3 == 0 || num_array.include?(3)
+     elsif number % 3 == 0 || num_array.include?(3) && num_array.uniq.length != 1
+       @return = 'fizz buzz'
       @return = 'fizz'
+    elsif num_array.uniq.length == 1
+
+       @return = 'deluxe'
      else
-       @return = number
+        @return = number
   end
 
 
+
   end
-  
-  if num_array.uniq.length == 1
-    @return += 'deluxe'
-  end
+
+
 end
+
 
 
 
